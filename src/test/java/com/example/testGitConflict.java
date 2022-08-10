87c085829f2dccd75c0201cccfe13db1abe88284
TimeQA <qaeducation2022@gmail.com>
2022-08-10 15:31:04 +0300

    first commit

---
+++ src/test/java/com/example/testGitConflict.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class testGitConflict {
+
+    @Test
+    void firstTest() {
+        Assertions.assertTrue(3 > 2);
+    }
+}
