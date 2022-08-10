07e45b79409f73f24262b6364ebda5a6bf08159d
TimeQA <qaeducation2022@gmail.com>
2022-08-10 18:30:39 +0300

    change operator to >=

--- src/test/java/com/example/TestGitConflict.java
+++ src/test/java/com/example/TestGitConflict.java
@@ -3,20 +3,20 @@ package com.example;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class testGitConflict {
+public class TestGitConflict {
 
     @Test
     void firstTest() {
-        Assertions.assertTrue(3 > 2);
+        Assertions.assertTrue(3 >= 2);
     }
 
     @Test
     void  secongTest() {
-        Assertions.assertTrue(3 > 0);
+        Assertions.assertTrue(3 >= 0);
     }
 
     @Test
     void thirdTest() {
-        Assertions.assertTrue(99 > 12);
+        Assertions.assertTrue(99 >= 12);
     }
 }
